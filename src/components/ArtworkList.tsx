aec1be923306584097ff5331b25e66335dbeecb3
euandecoup <151732440+euandecoup@users.noreply.github.com>
2024-10-12 13:29:10 +0100

    basic types interfaces created

--- src/components/ArtworkList.tsx
+++ src/components/ArtworkList.tsx
@@ -1,1 +1,32 @@
-export {}
+import React from "react";
+import { Artwork } from "../types/artwork";
+
+interface ArtworkListProps {
+  artworks: Artwork[];
+  onAddToExhibition: (artwork: Artwork) => void;
+}
+
+const ArtworkList: React.FC<ArtworkListProps> = ({
+  artworks,
+  onAddToExhibition,
+}) => {
+  return (
+    <div>
+      {artworks.length === 0 ? (
+        <p>No artworks found.</p>
+      ) : (
+        artworks.map((artwork) => (
+          <div key={artwork.id}>
+            <h3>{artwork.title}</h3>
+            <p>Artist: {artwork.artist}</p>
+            <button onClick={() => onAddToExhibition(artwork)}>
+              Add to Exhibition
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default ArtworkList;
