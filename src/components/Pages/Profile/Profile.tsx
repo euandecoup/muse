dbca158940f3445af009d05161c3fa2237504db5
euandecoup <151732440+euandecoup@users.noreply.github.com>
2024-10-26 15:42:19 +0100

    implemented routes with updated directory structure

---
+++ src/components/Pages/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Profile.module.css";
+
+const Profile: React.FC = () => (
+  <div className={styles.pageContainer}>
+    <h1>My Profile</h1>
+    {/* Profile content TCB */}
+  </div>
+);
+
+export default Profile;
---
+++ src/components/Pages/Settings/Settings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Settings.module.css";
+
+const Settings: React.FC = () => (
+  <div className={styles.pageContainer}>
+    <h1>Settings</h1>
+    {/* Settings content TBC */}
+  </div>
+);
+
+export default Settings;
